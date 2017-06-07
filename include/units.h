248b5ca01829b744ac4008246c17811d7f8b3bc3
Eivind Samuelsen <eivind.samuelsen@gmail.com>
2017-06-07 11:26:22 +0200

    Changed unit logic so that any operation that would return quantity<T, Unit::None> just returns T instead

--- include/units.h
+++ include/units.h
@@ -1,20 +1,41 @@
 #pragma once
 
 #include <ostream>
+#include <cmath>
+#include <type_traits>
 
+#pragma push_macro("TEMPLATE_QA_QB")
 #define TEMPLATE_QA_QB template <class A, class B, Unit UA, Unit UB>
+#pragma push_macro("TEMPLATE_A_QB")
+#define TEMPLATE_A_QB template <class A, class B, Unit UB, class = details::if_arithmetic_t<A>>
+#pragma push_macro("QA")
 #define QA quantity<A, UA>
+#pragma push_macro("QB")
 #define QB quantity<B, UB>
 
 namespace units
 {
-	enum class Unit : unsigned char { None, Distance, Time, Mass, Area, Volume, Speed, Density, Angle, AngularSpeed, Invalid };
+	enum class Unit : unsigned char { None, Distance, Time, Mass, Area, Volume, Speed, Density, Frequency, Invalid };
+
+	namespace details
+	{
+		template <class T, class R = void>
+		struct if_arithmetic : public std::enable_if<std::is_arithmetic_v<T>> { };
+		template <class T>
+		using if_arithmetic_t = typename if_arithmetic<T>::type;
+
+		inline constexpr unsigned code(Unit a) { return static_cast<unsigned>(a); }
+
+		// Set code, ordered
+		inline constexpr unsigned co(Unit a, Unit b) { return code(a)*code(Unit::Invalid) + code(b); }
+		// Set code, unordered
+		inline constexpr unsigned cu(Unit a, Unit b) { return a < b ? cu(b, a) : (code(a)*(code(a) + 1)) / 2 + code(b); }
+	}
 
 	inline std::ostream& operator<<(std::ostream& out, Unit u)
 	{
 		switch (u)
 		{
-		case Unit::None: return out;
 		case Unit::Distance: return out << "m";
 		case Unit::Time: return out << "s";
 		case Unit::Mass: return out << "kg";
@@ -22,8 +43,7 @@ namespace units
 		case Unit::Volume: return out << "m^3";
 		case Unit::Speed: return out << "m/s";
 		case Unit::Density: return out << "kg/m^3";
-		case Unit::Angle: return out << "rad";
-		case Unit::AngularSpeed: return out << "rad/s";
+		case Unit::Frequency: return out << "Hz";
 		default: 
 			return out << "{invalid unit}";
 		}
@@ -35,47 +55,52 @@ namespace units
 	template <Unit A, Unit B>
 	constexpr equal_test<A, B> require_equal = {};
 
-	constexpr inline unsigned operator|(Unit a, Unit b)
-	{ 
-		return (static_cast<unsigned>(a)*static_cast<unsigned>(Unit::Invalid)) + static_cast<unsigned>(b);
-	}
-
 	constexpr inline Unit operator*(Unit a, Unit b)
 	{
+		using namespace details;
 		if (a == Unit::Invalid || b == Unit::Invalid)
 			return Unit::Invalid;
+
 		if (a == Unit::None) return b;
 		if (b == Unit::None) return a;
 
-		switch (a | b)
+		switch (cu(a, b))
 		{
-		case Unit::Distance | Unit::Distance: return Unit::Area;
-		case Unit::Distance | Unit::Area: return Unit::Volume;
-		case Unit::Area | Unit::Distance: return Unit::Volume;
-		case Unit::Speed | Unit::Time: return Unit::Distance;
-		case Unit::Time | Unit::Speed: return Unit::Distance;
-		case Unit::AngularSpeed | Unit::Time: return Unit::Angle;
-		case Unit::Time | Unit::AngularSpeed: return Unit::Angle;
-			//case Unit::
+		case cu(Unit::Distance, Unit::Distance): return Unit::Area;
+		case cu(Unit::Distance, Unit::Area): return Unit::Volume;
+		case cu(Unit::Speed, Unit::Time): return Unit::Distance;
+		case cu(Unit::Distance, Unit::Frequency): return Unit::Speed;
+		case cu(Unit::Time, Unit::Frequency): return Unit::None;
 		default:
 			return Unit::Invalid;
 		}
 	}
 	constexpr inline Unit operator/(Unit a, Unit b)
 	{
+		using namespace details;
 		if (a == Unit::Invalid || b == Unit::Invalid)
 			return Unit::Invalid;
-		if (b == Unit::None) return a;
+
 		if (a == b) return Unit::None;
 
-		switch (a | b)
+		if (b == Unit::None) return a;
+		if (a == Unit::None) switch (b)
 		{
-		case Unit::Distance | Unit::Time: return Unit::Speed;
-		case Unit::Angle | Unit::Time: return Unit::AngularSpeed;
-		case Unit::Volume | Unit::Area: return Unit::Distance;
-		case Unit::Volume | Unit::Distance: return Unit::Area;
-		case Unit::Area | Unit::Distance: return Unit::Distance;
-		case Unit::Mass | Unit::Volume: return Unit::Density;
+		case Unit::Time: return Unit::Frequency;
+		case Unit::Frequency: return Unit::Time;
+		default: 
+			return Unit::Invalid;
+		}
+
+		switch (co(a, b))
+		{
+		case co(Unit::Distance, Unit::Time): return Unit::Speed;
+		case co(Unit::Speed, Unit::Distance): return Unit::Frequency;
+		case co(Unit::Distance, Unit::Speed): return Unit::Time;
+		case co(Unit::Volume, Unit::Area): return Unit::Distance;
+		case co(Unit::Volume, Unit::Distance): return Unit::Area;
+		case co(Unit::Area, Unit::Distance): return Unit::Distance;
+		case co(Unit::Mass, Unit::Volume): return Unit::Density;
 		default: 
 			return Unit::Invalid;
 		}
@@ -86,13 +111,14 @@ namespace units
 	{
 	public:
 		static_assert(std::is_arithmetic_v<T>, "T must be an arithmetic type");
-		static_assert(U != Unit::Invalid, "invalid unit");
+		static_assert(U != Unit::Invalid, "Invalid unit");
+		static_assert(U != Unit::None, "Logic error: Units::None should always result in raw arithmetic type");
 
 		T value;
 
 		quantity() { }
 
-		template <class S, class = std::enable_if<std::is_arithmetic_v<S>>>
+		template <class S, class = details::if_arithmetic_t<S>>
 		explicit constexpr quantity(S value) : value(T(value)) { }
 
 		T& operator*() { return value; }
@@ -108,6 +134,22 @@ namespace units
 		explicit constexpr operator double() const { return double(value); }
 	};
 
+	namespace details
+	{
+		template <Unit U>
+		struct maker
+		{
+			template <class T>
+			static constexpr auto make(T value) { return quantity<T, U>{ value }; }
+		};
+		template <>
+		struct maker<Unit::None>
+		{
+			template <class T>
+			static constexpr T make(T value) { return value; }
+		};
+	}
+
 	template <typename T, Unit U>
 	std::ostream& operator<<(std::ostream& out, const quantity<T, U>& q) { return out << q.value << U; }
 
@@ -115,25 +157,10 @@ namespace units
 	template <class A, Unit UA>
 	constexpr auto abs(quantity<A, UA> v) { return quantity<A, UA>{ std::abs(v.value) }; }
 
-	namespace details
-	{
-		template <class A, class B>
-		using add_type = decltype(std::declval<A>() + std::declval<B>());
-		template <class A, class B>
-		using sub_type = decltype(std::declval<A>() - std::declval<B>());
-		template <class A, class B>
-		using mul_type = decltype(std::declval<A>() * std::declval<B>());
-		template <class A, class B>
-		using div_type = decltype(std::declval<A>() / std::declval<B>());
-
-		template <class T>
-		using if_arithmetic_t = std::enable_if_t<std::is_arithmetic_v<T>>;
-	}
-
-	TEMPLATE_QA_QB constexpr auto operator+(QA a, QB b) { require_equal<UA, UB>; return quantity<details::add_type<A, B>, UA>{ a.value + b.value }; }
-	TEMPLATE_QA_QB constexpr auto operator-(QA a, QB b) { require_equal<UA, UB>; return quantity<details::sub_type<A, B>, UA>{ a.value - b.value }; }
-	TEMPLATE_QA_QB constexpr auto operator*(QA a, QB b) { return quantity<details::mul_type<A, B>, UA * UB>{ a.value * b.value }; }
-	TEMPLATE_QA_QB constexpr auto operator/(QA a, QB b) { return quantity<details::div_type<A, B>, UA / UB>{ a.value / b.value }; }
+	TEMPLATE_QA_QB constexpr auto operator+(QA a, QB b) { require_equal<UA, UB>; return details::maker<UA>::make(a.value + b.value); }
+	TEMPLATE_QA_QB constexpr auto operator-(QA a, QB b) { require_equal<UA, UB>; return details::maker<UA>::make(a.value - b.value); }
+	TEMPLATE_QA_QB constexpr auto operator*(QA a, QB b) { return details::maker<UA * UB>::make(a.value * b.value); }
+	TEMPLATE_QA_QB constexpr auto operator/(QA a, QB b) { return details::maker<UA / UB>::make(a.value / b.value); }
 
 	TEMPLATE_QA_QB constexpr bool operator!=(QA a, QB b) { require_equal<UA, UB>; return a.value != b.value; }
 	TEMPLATE_QA_QB constexpr bool operator< (QA a, QB b) { require_equal<UA, UB>; return a.value <  b.value; }
@@ -143,7 +170,6 @@ namespace units
 	TEMPLATE_QA_QB constexpr bool operator> (QA a, QB b) { require_equal<UA, UB>; return a.value >  b.value; }
 
 
-	template <class V> using NoUnit       = quantity<V, Unit::None>;
 	template <class V> using Distance     = quantity<V, Unit::Distance>;
 	template <class V> using Time         = quantity<V, Unit::Time>;
 	template <class V> using Mass         = quantity<V, Unit::Mass>;
@@ -151,13 +177,10 @@ namespace units
 	template <class V> using Volume       = quantity<V, Unit::Volume>;
 	template <class V> using Speed        = quantity<V, Unit::Speed>;
 	template <class V> using Density      = quantity<V, Unit::Density>;
-	template <class V> using Angle        = quantity<V, Unit::Angle>;
-	template <class V> using AngularSpeed = quantity<V, Unit::AngularSpeed>;
-
+	template <class V> using Frequency    = quantity<V, Unit::Frequency>;
 
 	namespace unit
 	{
-		template <class V> constexpr auto         none(V value) { return       NoUnit<V>{ value }; }
 		template <class V> constexpr auto     distance(V value) { return     Distance<V>{ value }; }
 		template <class V> constexpr auto         time(V value) { return         Time<V>{ value }; }
 		template <class V> constexpr auto         mass(V value) { return         Mass<V>{ value }; }
@@ -165,30 +188,26 @@ namespace units
 		template <class V> constexpr auto       volume(V value) { return       Volume<V>{ value }; }
 		template <class V> constexpr auto        speed(V value) { return        Speed<V>{ value }; }
 		template <class V> constexpr auto      density(V value) { return      Density<V>{ value }; }
-		template <class V> constexpr auto        angle(V value) { return        Angle<V>{ value }; }
-		template <class V> constexpr auto angularSpeed(V value) { return AngularSpeed<V>{ value }; }
 	}
 
-	template <class A, class B, Unit U, class = details::if_arithmetic_t<A>> constexpr auto operator*(A a, quantity<B, U> b) { return unit::none(a) * b; }
-	template <class A, class B, Unit U, class = details::if_arithmetic_t<B>> constexpr auto operator*(quantity<A, U> a, B b) { return a * unit::none(b); }
-	template <class A, class B, Unit U, class = details::if_arithmetic_t<A>> constexpr auto operator/(A a, quantity<B, U> b) { return unit::none(a) / b; }
-	template <class A, class B, Unit U, class = details::if_arithmetic_t<B>> constexpr auto operator/(quantity<A, U> a, B b) { return a / unit::none(b); }
+	TEMPLATE_A_QB constexpr auto operator*(A a, QB b) { return details::maker<UB>::make(a * b.value); }
+	TEMPLATE_A_QB constexpr auto operator*(QB b, A a) { return details::maker<UB>::make(b.value * a); }
+	TEMPLATE_A_QB constexpr auto operator/(A a, QB b) { return details::maker<Unit::None / UB>::make(a / b.value); }
+	TEMPLATE_A_QB constexpr auto operator/(QB b, A a) { return details::maker<UB>::make(b.value / a); }
 
 	namespace float_literals
 	{
-		inline constexpr auto operator"" _m(long double v) { return unit::distance(static_cast<float>(v)); }
-		inline constexpr auto operator"" _m2(long double v) { return unit::area(static_cast<float>(v)); }
-		inline constexpr auto operator"" _m3(long double v) { return unit::volume(static_cast<float>(v)); }
-		inline constexpr auto operator"" _kg(long double v) { return unit::mass(static_cast<float>(v)); }
-		inline constexpr auto operator"" _s(long double v) { return unit::time(static_cast<float>(v)); }
-		inline constexpr auto operator"" _mps(long double v) { return unit::speed(static_cast<float>(v)); }
+		inline constexpr auto operator"" _m    (long double v) { return unit::distance(static_cast<float>(v)); }
+		inline constexpr auto operator"" _m2   (long double v) { return unit::area(static_cast<float>(v)); }
+		inline constexpr auto operator"" _m3   (long double v) { return unit::volume(static_cast<float>(v)); }
+		inline constexpr auto operator"" _kg   (long double v) { return unit::mass(static_cast<float>(v)); }
+		inline constexpr auto operator"" _s    (long double v) { return unit::time(static_cast<float>(v)); }
+		inline constexpr auto operator"" _mps  (long double v) { return unit::speed(static_cast<float>(v)); }
 		inline constexpr auto operator"" _kgpm3(long double v) { return unit::density(static_cast<float>(v)); }
-		inline constexpr auto operator"" _rad(long double v) { return unit::angle(static_cast<float>(v)); }
-		inline constexpr auto operator"" _radps(long double v) { return unit::angularSpeed(static_cast<float>(v)); }
 
-		inline constexpr auto operator"" _mm(long double v) { return unit::distance(static_cast<float>(v / 1000)); }
-		inline constexpr auto operator"" _g(long double v) { return unit::distance(static_cast<float>(v / 1000)); }
-		inline constexpr auto operator"" _kgpl(long double v) { return unit::distance(static_cast<float>(v * 1000)); }
+		inline constexpr auto operator"" _mm  (long double v) { return unit::distance(static_cast<float>(v / 1000)); }
+		inline constexpr auto operator"" _g   (long double v) { return unit::mass(static_cast<float>(v / 1000)); }
+		inline constexpr auto operator"" _kgpl(long double v) { return unit::density(static_cast<float>(v * 1000)); }
 
 		static constexpr auto meter  = 1.0_m;
 		static constexpr auto second = 1.0_s;
@@ -200,16 +219,14 @@ namespace units
 		static constexpr float mega = 1e6f;
 	}
 
-	using std::sqrt;
 	template <class V>
-	auto sqrt(Area<V> a) { return Distance<V>(sqrt(a.value)); }
-	template <class V>
-	auto sqrt(NoUnit<V> a) { return NoUnit<V>(sqrt(a.value)); }
+	auto sqrt(Area<V> a) { return Distance<V>(std::sqrt(a.value)); }
 }
 
-#undef TEMPLATE_QA_QB
-#undef QA
-#undef QB
+#pragma pop_macro("TEMPLATE_QA_QB")
+#pragma pop_macro("TEMPLATE_A_QB")
+#pragma pop_macro("QA")
+#pragma pop_macro("QB")
 
 namespace uv
 {
