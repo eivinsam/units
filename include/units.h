8e2981762a902c3890998c08e124e3ef07c12e94
Eivind Samuelsen <eivind.samuelsen@gmail.com>
2017-06-09 12:07:16 +0200

    Changed implementation to use 7-component template in order to generalize for all powers of all SI units

--- include/units.h
+++ include/units.h
@@ -5,213 +5,186 @@
 #include <type_traits>
 
 #pragma push_macro("TEMPLATE_QA_QB")
-#define TEMPLATE_QA_QB template <class A, class B, Unit UA, Unit UB>
+#define TEMPLATE_QA_QB template <class A, class B, class UA, class UB>
 #pragma push_macro("TEMPLATE_A_QB")
-#define TEMPLATE_A_QB template <class A, class B, Unit UB, class = details::if_arithmetic_t<A>>
+#define TEMPLATE_A_QB template <class A, class B, class UB, class = details::if_arithmetic_t<A>>
+#pragma push_macro("ASSERT_SAME_UNIT")
+#define ASSERT_SAME_UNIT static_assert(std::is_same_v<UA, UB>, "Operation requires equal units")
 #pragma push_macro("QA")
-#define QA quantity<A, UA>
+#define QA Quantity<A, UA>
 #pragma push_macro("QB")
-#define QB quantity<B, UB>
+#define QB Quantity<B, UB>
 
 namespace units
 {
-	enum class Unit : unsigned char { None, Distance, Time, Mass, Area, Volume, Speed, Density, Frequency, Invalid };
+	namespace details
+	{
+		template <class T>
+		static constexpr bool always_false_v = false;
+
+		inline constexpr bool all() { return true; }
+		template <class... Rest>
+		inline constexpr bool all(bool first, Rest... rest) { return first && all(rest...); }
+	}
+
+	template <int m, int kg, int s, int A, int K, int mol, int cd>
+	struct Unit { };
+
+	using Ratio = Unit<0, 0, 0, 0, 0, 0, 0>;
+
+	template <int... AN, int... BN>
+	constexpr Unit<(AN + BN)...> operator*(Unit<AN...>, Unit<BN...>) { return {}; }
+	template <int... AN, int... BN>
+	constexpr Unit<(AN - BN)...> operator/(Unit<AN...>, Unit<BN...>) { return {}; }
+
+	template <int... AN>
+	constexpr Unit<(AN/2)...> sqrt(Unit<AN...>)
+	{
+		static_assert(details::all((AN%2 == 0)...), "Cannot take square root of odd-dimensioned Unit");
+		return {};
+	}
+	template <int... AN>
+	constexpr Unit<(AN * 2)...> square(Unit<AN...>) { return {}; }
+	template <int... AN>
+	constexpr Unit<(AN * 3)...> cube(Unit<AN...>) { return {}; }
+
+	static constexpr Unit<1, 0, 0, 0, 0, 0, 0> meter;
+	static constexpr Unit<0, 1, 0, 0, 0, 0, 0> kilogram;
+	static constexpr Unit<0, 0, 1, 0, 0, 0, 0> second;
+	static constexpr Unit<0, 0, 0, 1, 0, 0, 0> Ampere;
+	static constexpr Unit<0, 0, 0, 0, 1, 0, 0> Kelvin;
+
+	static constexpr Unit<0, 0, -1, 0, 0, 0, 0> Hertz;
+
+#pragma warning(push)
+#pragma warning(disable: 4127)
+	template <int m, int kg, int s, int A, int K, int mol, int cd>
+	inline std::ostream& operator<<(std::ostream& out, Unit<m, kg, s, A, K, mol, cd>)
+	{
+		auto print_unit = [&](int p, const char* unit) { if (p < 1) return; out << unit; if (p > 1) out << '^' << p; };
+		print_unit(m, "m");
+		print_unit(kg, "kg");
+		print_unit(s, "s");
+		print_unit(A, "A");
+		print_unit(K, "K");
+		print_unit(mol, "mol");
+		print_unit(cd, "cd");
+		if (m < 0 || kg < 0 || s < 0 || A < 0 || K < 0 || mol < 0 || cd < 0)
+			out << '/' << Unit<-m, -kg, -s, -A, -K, -mol, -cd>{};
+		return out;
+	}
+#pragma warning(pop)
+
+
+	static constexpr auto foo = sqrt(square(meter));
 
 	namespace details
 	{
+		template <class U>
+		struct is_unit : public std::false_type { };
+		template <int... UN>
+		struct is_unit<Unit<UN...>> : public std::true_type { };
+		template <class U>
+		static constexpr bool is_unit_v = is_unit<U>::value;
+
 		template <class T, class R = void>
 		struct if_arithmetic : public std::enable_if<std::is_arithmetic_v<T>> { };
 		template <class T>
 		using if_arithmetic_t = typename if_arithmetic<T>::type;
-
-		inline constexpr unsigned code(Unit a) { return static_cast<unsigned>(a); }
-
-		// Set code, ordered
-		inline constexpr unsigned co(Unit a, Unit b) { return code(a)*code(Unit::Invalid) + code(b); }
-		// Set code, unordered
-		inline constexpr unsigned cu(Unit a, Unit b) { return a < b ? cu(b, a) : (code(a)*(code(a) + 1)) / 2 + code(b); }
 	}
 
-	inline std::ostream& operator<<(std::ostream& out, Unit u)
+	template <class T, class U>
+	struct Quantity
 	{
-		switch (u)
-		{
-		case Unit::Distance: return out << "m";
-		case Unit::Time: return out << "s";
-		case Unit::Mass: return out << "kg";
-		case Unit::Area: return out << "m^2";
-		case Unit::Volume: return out << "m^3";
-		case Unit::Speed: return out << "m/s";
-		case Unit::Density: return out << "kg/m^3";
-		case Unit::Frequency: return out << "Hz";
-		default:
-			return out << "{invalid unit}";
-		}
-	}
-
-	template <Unit A, Unit B>
-	struct equal_test { static_assert(A == B, "operation requres equal units"); };
-
-	template <Unit A, Unit B>
-	constexpr equal_test<A, B> require_equal = {};
-
-
-	constexpr inline Unit operator*(Unit a, Unit b)
-	{
-		using namespace details;
-		if (a == Unit::Invalid || b == Unit::Invalid)
-			return Unit::Invalid;
-
-		if (a == Unit::None) return b;
-		if (b == Unit::None) return a;
-
-		switch (cu(a, b))
-		{
-		case cu(Unit::Distance, Unit::Distance): return Unit::Area;
-		case cu(Unit::Distance, Unit::Area): return Unit::Volume;
-		case cu(Unit::Speed, Unit::Time): return Unit::Distance;
-		case cu(Unit::Distance, Unit::Frequency): return Unit::Speed;
-		case cu(Unit::Time, Unit::Frequency): return Unit::None;
-		default:
-			return Unit::Invalid;
-		}
-	}
-	constexpr inline Unit operator/(Unit a, Unit b)
-	{
-		using namespace details;
-		if (a == Unit::Invalid || b == Unit::Invalid)
-			return Unit::Invalid;
-
-		if (a == b) return Unit::None;
-
-		if (b == Unit::None) return a;
-		if (a == Unit::None) switch (b)
-		{
-		case Unit::Time: return Unit::Frequency;
-		case Unit::Frequency: return Unit::Time;
-		default:
-			return Unit::Invalid;
-		}
-
-		switch (co(a, b))
-		{
-		case co(Unit::Distance, Unit::Time): return Unit::Speed;
-		case co(Unit::Speed, Unit::Distance): return Unit::Frequency;
-		case co(Unit::Distance, Unit::Speed): return Unit::Time;
-		case co(Unit::Volume, Unit::Area): return Unit::Distance;
-		case co(Unit::Volume, Unit::Distance): return Unit::Area;
-		case co(Unit::Area, Unit::Distance): return Unit::Distance;
-		case co(Unit::Mass, Unit::Volume): return Unit::Density;
-		default:
-			return Unit::Invalid;
-		}
-	}
-
-	template <Unit U>
-	struct Unity { static_assert(U != Unit::Invalid, "Invalid unit"); };
-
-	static constexpr Unity<Unit::Distance> meter;
-	static constexpr Unity<Unit::Time> second;
-	static constexpr Unity<Unit::Mass> kilogram;
-	static constexpr Unity<Unit::Frequency> hertz;
-
-	template <Unit UA, Unit UB>
-	inline constexpr Unity<UA * UB> operator*(Unity<UA>, Unity<UB>) { return {}; }
-	template <Unit UA, Unit UB>
-	inline constexpr Unity<UA / UB> operator/(Unity<UA>, Unity<UB>) { return {}; }
-
-	template <typename T, Unit U>
-	class quantity
-	{
-	public:
 		static_assert(std::is_arithmetic_v<T>, "T must be an arithmetic type");
-		static_assert(U != Unit::Invalid, "Invalid unit");
-		static_assert(U != Unit::None, "Logic error: Units::None should always result in raw arithmetic type");
+		static_assert( std::is_same_v<Ratio, decltype(U{} / U{})> , "U must be a Unit");
+		static_assert(!std::is_same_v<Ratio, U>, "Ratio unit should always result in raw arithmetic type");
 
 		T value;
 
-		quantity() { }
+		Quantity() { }
 
 		template <class S, class = details::if_arithmetic_t<S>>
-		explicit constexpr quantity(S value) : value(T(value)) { }
+		explicit constexpr Quantity(S value) : value(T(value)) { }
 
-		T& operator*() { return value; }
+		      T& operator*()       { return value; }
 		const T& operator*() const { return value; }
-		T* operator->() { return &value; }
-		const T* operator->() const { return &value; }
 
-		constexpr quantity& operator+=(const quantity& other) { value += other.value; return *this; }
-		constexpr quantity& operator-=(const quantity& other) { value -= other.value; return *this; }
+		constexpr Quantity& operator+=(const Quantity& other) { value += other.value; return *this; }
+		constexpr Quantity& operator-=(const Quantity& other) { value -= other.value; return *this; }
 
-		constexpr auto operator-() const { return quantity{ -value }; }
+		constexpr auto operator-() const { return Quantity{ -value }; }
 
 		explicit constexpr operator double() const { return double(value); }
 	};
 
+
 	namespace details
 	{
-		template <Unit U>
+		template <class U>
 		struct maker
 		{
 			template <class T>
-			static constexpr auto make(T value) { return quantity<T, U>{ value }; }
+			static constexpr auto make(T value) { return Quantity<T, std::remove_const_t<U>>{ value }; }
 		};
 		template <>
-		struct maker<Unit::None>
+		struct maker<Ratio>
 		{
 			template <class T>
 			static constexpr T make(T value) { return value; }
 		};
 	}
 
-	template <typename T, Unit U>
-	std::ostream& operator<<(std::ostream& out, const quantity<T, U>& q) { return out << q.value << U; }
+	template <typename T, class U>
+	std::ostream& operator<<(std::ostream& out, const Quantity<T, U>& q) { return out << q.value << U{}; }
 
 
-	template <class A, Unit UA>
-	constexpr auto abs(quantity<A, UA> v) { return quantity<A, UA>{ std::abs(v.value) }; }
+	template <class A, class UA>
+	constexpr auto abs(Quantity<A, UA> v) { return Quantity<A, UA>{ std::abs(v.value) }; }
 
-	TEMPLATE_QA_QB constexpr auto operator+(QA a, QB b) { require_equal<UA, UB>; return details::maker<UA>::make(a.value + b.value); }
-	TEMPLATE_QA_QB constexpr auto operator-(QA a, QB b) { require_equal<UA, UB>; return details::maker<UA>::make(a.value - b.value); }
-	TEMPLATE_QA_QB constexpr auto operator*(QA a, QB b) { return details::maker<UA * UB>::make(a.value * b.value); }
-	TEMPLATE_QA_QB constexpr auto operator/(QA a, QB b) { return details::maker<UA / UB>::make(a.value / b.value); }
+	TEMPLATE_QA_QB constexpr auto operator+(QA a, QB b) { ASSERT_SAME_UNIT; return details::maker<UA>::make(a.value + b.value); }
+	TEMPLATE_QA_QB constexpr auto operator-(QA a, QB b) { ASSERT_SAME_UNIT; return details::maker<UA>::make(a.value - b.value); }
+	TEMPLATE_QA_QB constexpr auto operator*(QA a, QB b) { return details::maker<decltype(UA{} * UB{})>::make(a.value * b.value); }
+	TEMPLATE_QA_QB constexpr auto operator/(QA a, QB b) { return details::maker<decltype(UA{} / UB{})>::make(a.value / b.value); }
 
-	TEMPLATE_QA_QB constexpr bool operator!=(QA a, QB b) { require_equal<UA, UB>; return a.value != b.value; }
-	TEMPLATE_QA_QB constexpr bool operator< (QA a, QB b) { require_equal<UA, UB>; return a.value <  b.value; }
-	TEMPLATE_QA_QB constexpr bool operator<=(QA a, QB b) { require_equal<UA, UB>; return a.value <= b.value; }
-	TEMPLATE_QA_QB constexpr bool operator==(QA a, QB b) { require_equal<UA, UB>; return a.value == b.value; }
-	TEMPLATE_QA_QB constexpr bool operator>=(QA a, QB b) { require_equal<UA, UB>; return a.value >= b.value; }
-	TEMPLATE_QA_QB constexpr bool operator> (QA a, QB b) { require_equal<UA, UB>; return a.value >  b.value; }
+	TEMPLATE_QA_QB constexpr bool operator!=(QA a, QB b) { ASSERT_SAME_UNIT; return a.value != b.value; }
+	TEMPLATE_QA_QB constexpr bool operator< (QA a, QB b) { ASSERT_SAME_UNIT; return a.value <  b.value; }
+	TEMPLATE_QA_QB constexpr bool operator<=(QA a, QB b) { ASSERT_SAME_UNIT; return a.value <= b.value; }
+	TEMPLATE_QA_QB constexpr bool operator==(QA a, QB b) { ASSERT_SAME_UNIT; return a.value == b.value; }
+	TEMPLATE_QA_QB constexpr bool operator>=(QA a, QB b) { ASSERT_SAME_UNIT; return a.value >= b.value; }
+	TEMPLATE_QA_QB constexpr bool operator> (QA a, QB b) { ASSERT_SAME_UNIT; return a.value >  b.value; }
 
 
-	template <class V> using Distance     = quantity<V, Unit::Distance>;
-	template <class V> using Time         = quantity<V, Unit::Time>;
-	template <class V> using Mass         = quantity<V, Unit::Mass>;
-	template <class V> using Area         = quantity<V, Unit::Area>;
-	template <class V> using Volume       = quantity<V, Unit::Volume>;
-	template <class V> using Speed        = quantity<V, Unit::Speed>;
-	template <class V> using Density      = quantity<V, Unit::Density>;
-	template <class V> using Frequency    = quantity<V, Unit::Frequency>;
+	template <class V> using Distance     = Quantity<V, std::remove_const_t<decltype(meter)>>;
+	template <class V> using Time         = Quantity<V, std::remove_const_t<decltype(second)>>;
+	template <class V> using Mass         = Quantity<V, std::remove_const_t<decltype(kilogram)>>;
+	template <class V> using Area         = Quantity<V, std::remove_const_t<decltype(square(meter))>>;
+	template <class V> using Volume       = Quantity<V, std::remove_const_t<decltype(cube(meter))>>;
+	template <class V> using Speed        = Quantity<V, std::remove_const_t<decltype(meter / second)>>;
+	template <class V> using Density      = Quantity<V, std::remove_const_t<decltype(kilogram / cube(meter))>>;
+	template <class V> using Frequency    = Quantity<V, std::remove_const_t<decltype(Hertz)>>;
 
-	template <class T, Unit U, class = details::if_arithmetic_t<T>>
-	inline constexpr auto operator*(T value, Unity<U>) { return details::maker<U>::make(value); }
-	template <class T, Unit U, class = details::if_arithmetic_t<T>>
-	inline constexpr auto operator/(T value, Unity<U>) { return details::maker<Unit::None / U>::make(value); }
-	template <class A, Unit UA, Unit UB>
-	inline constexpr auto operator*(quantity<A, UA> a, Unity<UB>) { return details::maker<UA * UB>::make(a.value); }
-	template <class A, Unit UA, Unit UB>
-	inline constexpr auto operator/(quantity<A, UA> a, Unity<UB>) { return details::maker<UA / UB>::make(a.value); }
+	template <class T, int... UN, class = details::if_arithmetic_t<T>>
+	inline constexpr auto operator*(T value, Unit<UN...>) { return details::maker<Unit<UN...>>::make(value); }
+	template <class T, int... UN, class = details::if_arithmetic_t<T>>
+	inline constexpr auto operator/(T value, Unit<UN...>) { return details::maker<Unit<-UN...>>::make(value); }
+	template <class A, class UA, int... BN>
+	inline constexpr auto operator*(QA a, Unit<BN...>) { return details::maker<decltype(UA{} * Unit<BN...>{})> ::make(a.value); }
+	template <class A, class UA, int... BN>
+	inline constexpr auto operator/(QA a, Unit<BN...>) { return details::maker<decltype(UA{} / Unit<BN...>{})> ::make(a.value); }
 
 	TEMPLATE_A_QB constexpr auto operator*(A a, QB b) { return details::maker<UB>::make(a * b.value); }
 	TEMPLATE_A_QB constexpr auto operator*(QB b, A a) { return details::maker<UB>::make(b.value * a); }
-	TEMPLATE_A_QB constexpr auto operator/(A a, QB b) { return details::maker<Unit::None / UB>::make(a / b.value); }
+	TEMPLATE_A_QB constexpr auto operator/(A a, QB b) { return details::maker<decltype(Ratio{} / UB{})> ::make(a / b.value); }
 	TEMPLATE_A_QB constexpr auto operator/(QB b, A a) { return details::maker<UB>::make(b.value / a); }
+
+	template <class A, class UA> constexpr auto sqrt(QA a) { return details::maker<decltype(sqrt(UA{}))>::make(std::sqrt(a.value)); }
 
 	namespace float_literals
 	{
 		inline constexpr auto operator"" _m    (long double v) { return static_cast<float>(v)*meter; }
-		inline constexpr auto operator"" _m2   (long double v) { return static_cast<float>(v)*meter*meter; }
-		inline constexpr auto operator"" _m3   (long double v) { return static_cast<float>(v)*meter*meter*meter; }
+		inline constexpr auto operator"" _m2   (long double v) { return static_cast<float>(v)*square(meter); }
+		inline constexpr auto operator"" _m3   (long double v) { return static_cast<float>(v)*cube(meter); }
 		inline constexpr auto operator"" _kg   (long double v) { return static_cast<float>(v)*kilogram; }
 		inline constexpr auto operator"" _s    (long double v) { return static_cast<float>(v)*second; }
 		inline constexpr auto operator"" _mps  (long double v) { return static_cast<float>(v)*meter/second; }
@@ -226,13 +199,11 @@ namespace units
 		static constexpr float kilo = 1e3f;
 		static constexpr float mega = 1e6f;
 	}
-
-	template <class V>
-	auto sqrt(Area<V> a) { return Distance<V>(std::sqrt(a.value)); }
 }
 
 #pragma pop_macro("TEMPLATE_QA_QB")
 #pragma pop_macro("TEMPLATE_A_QB")
+#pragma pop_macro("ASSERT_SAME_UNIT")
 #pragma pop_macro("QA")
 #pragma pop_macro("QB")
 
@@ -241,9 +212,9 @@ namespace uv
 	template <class T>
 	struct is_scalar;
 
-	template <units::Unit U>
-	struct is_scalar<units::Unity<U>> : public std::true_type { };
+	template <int... UN>
+	struct is_scalar<units::Unit<UN...>> : public std::true_type { };
 
-	template <class T, units::Unit U>
-	struct is_scalar<units::quantity<T, U>> : public std::true_type { };
+	template <class T, class U>
+	struct is_scalar<units::Quantity<T, U>> : public std::true_type { };
 }
